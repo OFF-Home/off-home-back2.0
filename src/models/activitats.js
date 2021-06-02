74ac0e3c3d4b6f14f4d88f6bb7a01e10643d2a8e
agnes.masip <agnes.masip@estudiantat.upc.edu>
2021-06-03 00:19:24 +0200

    valorar

--- src/models/activitats.js
+++ src/models/activitats.js
@@ -575,18 +575,21 @@ exports.valorarActivitat= function(data,req,res,next) {
     let comentari;
     if (data.comentari == null ) comentari = null
     else comentari = data.comentari
-    let sql= 'SELECT * FROM Participants WHERE usuariCreador = ? AND dataHoraIni = ? AND usuariParticipant = ? AND valoracio is null;'
+    let sql= 'SELECT * FROM Participants WHERE usuariCreador = ? AND dataHoraIni = ? AND usuariParticipant = ?;'
     var valorar =0
     var result = [];
-    db.all(sql,[data.usuariCreador,data.dataHoraIni,data.usuariParticipant], (err,rows)=> {
+    db.get(sql,[data.usuariCreador,data.dataHoraIni,data.usuariParticipant], (err,row)=> {
 
         if (err) {
             res.status(409).json({
                 status: err.status,
                 message: err.message
             });
-        } else if (rows.length == 0) {
-            res.status(404).send('Activitat ja valorada');
+        } else if (row == null) {
+            res.status(404).send('Activitat no existeix');
+        }
+        else if(row.valoracio != null) {
+            res.status(409).send('Activitat ja valorada');
         }
         else {
             let sql2 = 'UPDATE Participants SET valoracio = ? , comentari = ? WHERE usuariCreador = ? AND dataHoraIni = ? AND usuariParticipant = ?;'
