2ff99e8bd9b341477ac331c63cc3119ff93774c8
victorferv <victor.fernandez.vazquez@estudiantat.upc.edu>
2021-06-04 13:12:11 +0200

    Fixing testing activitats

--- test/activitats_testing.js
+++ test/activitats_testing.js
@@ -14,12 +14,10 @@ describe('/GET SearchByRadi:', () => {
         db.serialize(() => {
             let sql = 'INSERT INTO Usuaris VALUES (?,?,?,?,?,?,?,?,?,?,?,?)';
             let sql2 = 'INSERT INTO Llocs VALUES (?,?,?,?)';
-            let sql3 = 'INSERT INTO Categories VALUES (?,?)';
             let sql4 = 'INSERT INTO Activitats VALUES (?,?,?,?,?,?,?,?,?)';
             db.run(sql,['josep@gmail.com','josep','2365']);
             db.run(sql2,['C/Font',25,41.401703,2.175327])
-            db.run(sql3,['Training','Train']);
-            db.run(sql4,['josep@gmail.com','C/Font',25,'19-04-2021 18:00:00','Training',10,'Play','playing','19-04-2021 19:00:00'], () => {
+            db.run(sql4,['josep@gmail.com','C/Font',25,'19-04-2021 18:00:00','Running',10,'Play','playing','19-04-2021 19:00:00'], () => {
                 done();
             });
         });
@@ -29,18 +27,15 @@ describe('/GET SearchByRadi:', () => {
             let sql = 'DELETE FROM Activitats WHERE usuariCreador = ? and dataHoraIni = ?';
             let sql2= 'DELETE FROM Usuaris WHERE username = ?;';
             let sql3 = 'DELETE FROM Llocs WHERE nomCarrer = ? and numCarrer = ?';
-            let sql4 = 'DELETE FROM Categories WHERE categoria = ?';
             db.run(sql,['josep@gmail.com','19-04-2021 18:00:00']);
             db.run(sql2,['josep']);
             db.run(sql3,['C/Font',25]);
-            db.run(sql4,['Training']);
         })
         done();
     });
     it('should return the activity', (done) => {
         chai.request(url)
-            .get('/activitats/searchbyradi')
-            .send({latitud:41.400922, altitud:2.174286, distance:3})
+            .get('/activitats/searchbyradi?latitud=41.400922&altitud=2.174286&distance=3')
             .end(function(err,res) {
                 expect(res).to.have.status(200);
                 expect(res.body).to.have.an('array');
@@ -49,8 +44,7 @@ describe('/GET SearchByRadi:', () => {
     });
     it('should not return anything', (done) => {
         chai.request(url)
-            .get('/activitats/searchbyradi')
-            .send({latitud:41.400922, altitud:2.174286, distance:0.1})
+            .get('/activitats/searchbyradilatitud=41.400922&altitud=2.174286&distance=0.1')
             .end(function(err,res) {
                 expect(res).to.have.status(204);
                 done();
@@ -66,11 +60,9 @@ describe('/GET Participants Activitat:', () => {
             db.run(sql, ['jess2@gmail.com', 'jess2', '56567']);
             sql = 'INSERT INTO Llocs VALUES (?,?,?,?)';
             db.run(sql, ['C/Font', 25, 41.401703, 2.175327]);
-            sql = 'INSERT INTO Categories VALUES (?,?)'
-            db.run(sql, ['Train', 'Trai']);
             sql = 'INSERT INTO Activitats VALUES (?,?,?,?,?,?,?,?,?)';
-            db.run(sql, ['jess@gmail.com', 'C/Font', 25, '19-04-2021 18:00:00', 'Train', 10, 'Play', 'playing', '19-04-2021 19:00:00']);
-            db.run(sql, ['jess@gmail.com', 'C/Font', 25, '30-04-2021 18:00:00', 'Train', 10, 'Play', 'playing', '30-04-2021 19:00:00']);
+            db.run(sql, ['jess@gmail.com', 'C/Font', 25, '19-04-2021 18:00:00', 'Running', 10, 'Play', 'playing', '19-04-2021 19:00:00']);
+            db.run(sql, ['jess@gmail.com', 'C/Font', 25, '30-04-2021 18:00:00', 'Running', 10, 'Play', 'playing', '30-04-2021 19:00:00']);
             sql = 'INSERT INTO Participants VALUES (?,?,?,?,?)';
             db.run(sql,[null,'jess@gmail.com','19-04-2021 18:00:00','jess2@gmail.com',null],() => {
                 done();
@@ -89,8 +81,6 @@ describe('/GET Participants Activitat:', () => {
             db.run(sql, ['jess2']);
             sql = 'DELETE FROM Llocs WHERE nomCarrer = ? and numCarrer = ?';
             db.run(sql, ['C/Font', 25]);
-            sql = 'DELETE FROM Categories WHERE categoria = ?';
-            db.run(sql, ['Train']);
         })
         done();
     });
